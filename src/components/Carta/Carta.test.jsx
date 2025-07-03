0dedccf67deaef8f92d8cf3d7dbfe0c226c7e186
Raphael Ramalho <raphael.ramalho@outlook.com>
2025-07-03 15:24:00 -0300

    refactor: renomear prop 'emoji' para 'naipe' no componente Carta

--- src/components/Carta/Carta.test.jsx
+++ src/components/Carta/Carta.test.jsx
@@ -3,27 +3,27 @@ import { describe, expect, it, vi } from "vitest";
 import Carta from ".";
 
 describe("Componente Carta", () => {
-  it("deve exibir o emoji 💻 se estiver virada", () => {
-    render(<Carta emoji="💻" virada={true} aoClicar={() => {}} />);
+  it("deve exibir o naipe 💻 se estiver virada", () => {
+    render(<Carta naipe="💻" virada={true} aoClicar={() => {}} />);
     expect(screen.getByText("💻")).toBeInTheDocument();
   });
   it("deve exibir ❓ se estiver não virada", () => {
-    render(<Carta emoji="💻" virada={false} aoClicar={() => {}} />);
+    render(<Carta naipe="💻" virada={false} aoClicar={() => {}} />);
     expect(screen.getByText("❓")).toBeInTheDocument();
   });
   it("deve dispara ao clicar na carta", () => {
     const funcaoMocada = vi.fn(); //funcao falsa (mockada)
-    render(<Carta emoji="💻" virada={true} aoClicar={funcaoMocada} />);
+    render(<Carta naipe="💻" virada={true} aoClicar={funcaoMocada} />);
     screen.getByText("💻").click(); //simula click na carta
     expect(funcaoMocada).toHaveBeenCalled();
   });
   it("deve ter a classe 'virada'se virada for true", () => {
-    render(<Carta emoji="💻" virada={true} aoClicar={() => {}} />);
+    render(<Carta naipe="💻" virada={true} aoClicar={() => {}} />);
     const elemento = screen.getByTestId("carta-rotativa");
     expect(elemento.classList.contains("virada")).toBe(true);
   });
   it("não deve ter a classe 'virada' se virada for false", () => {
-    render(<Carta emoji="💻" virada={false} aoClicar={() => {}} />);
+    render(<Carta naipe="💻" virada={false} aoClicar={() => {}} />);
     const elemento = screen.getByTestId("carta-rotativa");
     expect(elemento.classList.contains("virada")).toBe(false);
   });
--- src/components/Carta/index.jsx
+++ src/components/Carta/index.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
 import "./Carta.css";
 
-export default function Carta({ emoji, virada, aoClicar }) {
+export default function Carta({ naipe, virada, aoClicar }) {
   return (
     <div className="carta-externa-fixo" onClick={aoClicar}>
-      <div className={`carta-interna-rotativo ${virada ? "virada" : ""}`}>
-        <div className="carta-frente">{emoji}</div>
+      <div
+        className={`carta-interna-rotativo ${virada ? "virada" : ""}`}
+        data-testid={`carta-rotativa`}
+      >
+        <div className="carta-frente">{naipe}</div>
         <div className="carta-verso">❓</div>
       </div>
     </div>
